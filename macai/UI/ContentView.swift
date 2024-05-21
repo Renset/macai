8269fef975064fdb4ddc6fd9651765306c00a86c
Renat Notfullin <renat@notfullin.com>
2024-05-22 00:29:13 +0200

    🛠️ Use default model from constants in ContentView (chat)

--- macai/UI/ContentView.swift
+++ macai/UI/ContentView.swift
@@ -23,7 +23,7 @@ struct ContentView: View {
     private var chats: FetchedResults<ChatEntity>
     @State var selectedChat: ChatEntity?
     @AppStorage("gptToken") var gptToken = ""
-    @AppStorage("gptModel") var gptModel = "gpt-3.5-turbo"
+    @AppStorage("gptModel") var gptModel = AppConstants.chatGptDefaultModel
     @AppStorage("systemMessage") var systemMessage = AppConstants.chatGptSystemMessage
     @AppStorage("lastOpenedChatId") var lastOpenedChatId = ""
     @AppStorage("apiUrl") var apiUrl = AppConstants.apiUrlChatCompletions
